7a32040f75dc26e99f5723b659e72db3fe0c242b
yaeli0686 <yaeli0686@gmail.com>
2021-10-03 20:08:45 +0300

    adding loading=lazy

--- src/components/userCard.jsx
+++ src/components/userCard.jsx
@@ -13,7 +13,7 @@ const UserCard = ({ user }) => {
         <div className="col-md-6 col-lg-4 col-xl-3 mb-4">
             <div className="userCard bg-light shadow rounded-3 overflow-hidden p-4 position-relative">
                 <div className="cardBG position-absolute" style={{ backgroundColor: randomColor() }}></div>
-                <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} className="d-block mx-auto my-4 rounded-circle shadow" />
+                <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} className="d-block mx-auto my-4 rounded-circle shadow" loading="lazy" />
                 <h6 className="nameWrapper d-flex justify-content-between gap-3">
                     <span className="name text-truncate" title={`${user.name.title} ${user.name.first} ${user.name.last}`}>{`${user.name.title} ${user.name.first} ${user.name.last}`}</span>
                     <span className="age">{user.dob.age}</span>
